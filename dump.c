b84f613aec3f6e6754a7e689052d846c4dd760a2
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2026-07-14 13:17:22 +0300

    dump. dump? dump!

---
+++ dump.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<ctype.h>
+c,j;main(a,v)const char**v;{ // classic | ascii ekleyeceğim sonrasında
+    FILE*i=fopen(v[1],"rb"),*o=fopen(v[2],"w");
+    for(;~(c=fgetc(i));j++){if(j%80==0)fprintf(o,"\n");
+        fprintf(o,"%c",(isalnum(c)||ispunct(c))?c:'.');}
+    fclose(i);fclose(o);
+}
